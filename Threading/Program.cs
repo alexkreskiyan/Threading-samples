b2d3470b7da2c6191220f622599594342da6ed9d
Alex Kreskiyan <a.kreskiyan@gmail.com>
2015-11-25 00:28:48 +0300

    Added two way signals sample

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -1,20 +1,33 @@
 ﻿using System;
+using System.Threading;
 
 namespace Threading
 {
     internal class Program
     {
-        static object stdoutLocker = new object();
+        private static object stdoutLocker = new object();
+
         private static void Main(string[] args)
         {
-            ThreadingAutoResetEvent.Test.Run();
+            ThreadingTwoWaySignals.Test.Run();
         }
 
         public static void WriteLine(string message)
         {
+            Write(message);
+        }
+
+        public static void WriteLine(string format, params object[] args)
+        {
+            Write(string.Format(format, args));
+        }
+
+        private static void Write(string message)
+        {
             lock (stdoutLocker)
             {
-                Console.Write(DateTime.Now.ToString("HH:mm:ss.ffff: "));
+                Console.Write(DateTime.Now.ToString("HH:mm:ss.ffff "));
+                Console.Write("{0,-10} ", string.Concat('[', Thread.CurrentThread.Name, ']'));
                 Console.WriteLine(message);
             }
         }
---
+++ Threading/TestTwoWaySignals.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading;
+using Threading;
+
+namespace ThreadingTwoWaySignals
+{
+    internal class Test
+    {
+        static EventWaitHandle worker = new AutoResetEvent(false);
+        static EventWaitHandle main = new AutoResetEvent(false);
+        static readonly object locker = new object();
+        static string message;
+
+        private static EventWaitHandle waitHandle = new AutoResetEvent(false);
+
+        public static void Run()
+        {
+            Thread.CurrentThread.Name = "Main";
+            Program.WriteLine("Start");
+
+            new Thread(Work) { Name = "Worker" }.Start();
+
+            worker.WaitOne();                           //wait worker
+            lock (locker) message = "aaa";
+            Program.WriteLine("{0} set message: {1}", Thread.CurrentThread.Name, message);
+            main.Set();                                 //tell worker to go
+
+            worker.WaitOne();
+            lock (locker) message = "bbb";               //give another message to worker
+            Program.WriteLine("{0} set message: {1}", Thread.CurrentThread.Name, message);
+            main.Set();
+
+            worker.WaitOne();
+            lock (locker) message = null;               //signal worker to exit
+            main.Set();
+
+            worker.WaitOne();
+            Program.WriteLine("End");
+        }
+
+        private static void Work()
+        {
+            while (true)
+            {
+                worker.Set();
+                main.WaitOne();
+                lock (locker)
+                {
+                    if (message == null)
+                    {
+                        Program.WriteLine("Worker received signal to exit");
+                        worker.Set();
+                        return;
+                    }
+
+                    Program.WriteLine("Received: {0}", message);
+                }
+            }
+        }
+    }
+}
