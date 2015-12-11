11d78b85d7644f22a9501d5fee08816a1fb58a48
Alex Kreskiyan <a.kreskiyan@gmail.com>
2015-12-11 14:08:24 +0300

    Simple BackgroundWorker sample added

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -10,7 +10,7 @@ namespace Threading
         private static void Main(string[] args)
         {
             Thread.CurrentThread.Name = "Main";
-            ThreadingEAPWebClient.Test.Run();
+            ThreadingBackgroundWorkerSimple.Test.Run();
         }
 
         public static void WriteLine(string message)
---
+++ Threading/ThreadingBackgroundWorkerSimple.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using Threading;
+
+namespace ThreadingBackgroundWorkerSimple
+{
+    internal class Test
+    {
+        private static BackgroundWorker worker = new BackgroundWorker();
+        private static ManualResetEvent waitHandle = new ManualResetEvent(false);
+
+        public static void Run()
+        {
+            Program.WriteLine("Add some work to worker");
+            worker.DoWork += DoWork;
+
+            Program.WriteLine("Run worker async");
+            worker.RunWorkerAsync("some work");
+            worker.RunWorkerCompleted += (sender, e) =>
+            {
+                Program.WriteLine("Job ended with result {0}", e.Result);
+                waitHandle.Set();
+            };
+
+            Program.WriteLine("Wait for waithandle set to end...");
+            waitHandle.WaitOne();
+        }
+
+        private static void DoWork(object sender, DoWorkEventArgs e)
+        {
+            Program.WriteLine("Working with {0}", e.Argument);
+            Thread.Sleep(500);
+            Program.WriteLine("Working with {0} finished", e.Argument);
+        }
+    }
+}
