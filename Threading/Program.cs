242328ba9146555307cea8b9cbaf6c1fe69dcd89
Alex Kreskiyan <a.kreskiyan@gmail.com>
2015-12-11 13:18:47 +0300

    EAP-based WebClient sample added

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -10,7 +10,7 @@ namespace Threading
         private static void Main(string[] args)
         {
             Thread.CurrentThread.Name = "Main";
-            ThreadingSynchronizationAttribute.Test.Run();
+            ThreadingEAPWebClient.Test.Run();
         }
 
         public static void WriteLine(string message)
---
+++ Threading/ThreadingEAPWebclient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using Threading;
+
+namespace ThreadingEAPWebClient
+{
+    internal class Test
+    {
+        public static void Run()
+        {
+            var wc = new WebClient();
+            wc.DownloadDataCompleted += (sender, args) =>
+              {
+                  if (args.Cancelled)
+                      Program.WriteLine("Canceled");
+                  else if (args.Error != null)
+                      Program.WriteLine("Exception: {0}", args.Error.Message);
+                  else
+                      Program.WriteLine("{0} chars downloaded", args.Result.Length);
+
+              };
+            wc.DownloadDataAsync(new Uri("http://alex-nl.azurewebsites.net/"));
+            Thread.Sleep(1000);
+        }
+    }
+}
