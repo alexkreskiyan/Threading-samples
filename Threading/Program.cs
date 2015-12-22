620ed8862d48e53af858812bb7d61a1f1c966c73
Alex Kreskiyan <a.kreskiyan@gmail.com>
2015-12-22 20:26:42 +0300

    Barrier sample added

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -10,7 +10,7 @@ namespace Threading
         private static void Main(string[] args)
         {
             Thread.CurrentThread.Name = "Main";
-            ThreadingWaitPulseWorker.Test.Run();
+            ThreadingBarrier.Test.Run();
         }
 
         public static void WriteLine(string message)
---
+++ Threading/ThreadingBarrier.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using Threading;
+
+namespace ThreadingBarrier
+{
+    internal class Test
+    {
+        private static Barrier Barrier = new Barrier(5);
+
+        public static void Run()
+        {
+            var words = 5;
+            for (var i = 0; i < Barrier.ParticipantCount; i++)
+                new Thread(Speak) { Name = "Worker#" + i }.Start(words);
+        }
+
+        private static void Speak(object raw)
+        {
+            int count = (int)raw;
+            for (var i = 0; i < count; i++)
+            {
+                Program.WriteLine(" {0}", i);
+                Barrier.SignalAndWait();
+            }
+        }
+    }
+}
